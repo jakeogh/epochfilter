807a7e2f42163640793c81f2a049762c398f668f
jakeogh <github.com@v6y.net>
2020-11-14 12:08:23 -0700

    auto-commit

--- epochfilter/epochfilter.py
+++ epochfilter/epochfilter.py
@@ -73,7 +73,7 @@ def cli(timestamps,
                                             debug=debug,
                                             verbose=verbose):
 
-        timestamp = decimal.decimal(timestamp)
+        timestamp = decimal.Decimal(timestamp)
         if verbose:
             ic(index, timestamp)
         if count:
